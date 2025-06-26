a6181a9e7a4a92d5ecb0cc7048e193deecc543aa
parrais <github@parrais.co.uk>
2025-06-26 10:32:17 +0100

    Improve header navigation and comment display

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 
 function Header() {
   const { username } = useContext(UserContext);
   return (
     <header>
-      <h1>MTL News</h1>
+      <h1>
+        {" "}
+        <Link to="/">MTL News</Link>
+      </h1>
       <p>News For Your Community</p>
       <p>Logged in as: {username}</p>
     </header>
