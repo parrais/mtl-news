943fac267d01f4d989f3c097a294f18ca046927c
parrais <github@parrais.co.uk>
2025-09-02 10:50:07 +0100

    Update title and favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer>
-      <Link to="/">Home</Link>
+      <p>
+        <Link to="/">Home</Link>
+      </p>
+
+      <p>
+        MTL News layout by <a href="https://github.com/parrais/">Matt Lewis</a>.
+        Site icon by <a href="http://www.freepik.com/">Freepik</a>.
+      </p>
     </footer>
   );
 }
